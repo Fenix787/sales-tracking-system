3dcd1fa93547d33431bb578a873f5ba1ee07a0d4
Kevin Clarke <fenix88@gmail.com>
2016-04-21 14:31:11 -0500

    v0.3.4 fixed navbar link generation

--- php/navbar.php
+++ php/navbar.php
@@ -7,12 +7,14 @@
         </div>
         <ul class="nav navbar-nav">
             <?php
-            if ($_SESSION['salesrep']->id == -1) {
+            $checkCust = isset($_POST['inputCustomer']) ? $_POST['inputCustomer'] : '';
+            $checkDo = isset($_REQUEST['do']) ? $_REQUEST['do'] : '';
+            if ($_SESSION['salesrep']->id == -1 || $checkDo == 'logout') {
                 echo '<li><a href="index.php?action=authSalesRep">Login</a></li>';
             }
             else {
                 echo '<li><a href="index.php?action=authSalesRep&do=logout">Logout</a></li>';
-                if ($_SESSION['quote']->id == -1) {
+                if ($_SESSION['quote']->id == -1 && $checkCust == '') {
                     echo '<li><a href="index.php?action=selectCust">Create New Quote</a></li>';
                 }
                 else {
