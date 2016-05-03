807c75240c60bc12c5f8d91173b32e7775f9e0a8
fenix <fenix88@gmail.com>
2016-05-03 13:14:34 -0500

    v0.4.7 fixed charset issues when running on babbage.cs.niu.edu

--- php/header.php
+++ php/header.php
@@ -1,9 +1,10 @@
 <?php session_start();
-$version = "v0.4.2"; ?>
+header('Content-type: text/html; charset=latin1_swedish_ci');
+$version = "v0.4.7"; ?>
 <!DOCTYPE html>
-<html lang="en">
+<html>
     <head>
-        <meta charset="latin1_swedish_ci">
+        <meta http-equiv="Content-Type" content="text/html;charset=latin1_swedish_ci">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
